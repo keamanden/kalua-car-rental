7948f41b85b756881fc139b390202f497b7f6e9d
Keamanden <than0002@stud.kea.dk>
2025-09-16 13:23:11 +0200

    autoincrement

--- cars.sql
+++ cars.sql
@@ -18,4 +18,17 @@ INSERT INTO cars (Brand, Model, FuelType, Registration, FirstRegistration, Milea
 ('Tesla', 'Model 3', 'Electric', 'TES456', '2021-01-10', 12000);
 
 
+INSERT INTO cars (Brand, Model, FuelType, Registration, FirstRegistration, Mileage) VALUES
+('Ford', 'Focus', 'Petrol', 'FOR123', '2018-05-10', 52000),
+('BMW', 'X3', 'Diesel', 'BMW456', '2017-11-03', 67000),
+('Hyundai', 'Ioniq 5', 'Electric', 'HYU789', '2022-06-18', 8000),
+('Audi', 'A4', 'Petrol', 'AUD321', '2019-09-25', 43000),
+('Kia', 'Sportage', 'Diesel', 'KIA654', '2020-02-14', 31000),
+('Mercedes-Benz', 'C-Class', 'Petrol', 'MER987', '2016-08-30', 75000),
+('Nissan', 'Leaf', 'Electric', 'NIS159', '2021-04-12', 15000),
+('Peugeot', '208', 'Petrol', 'PEU753', '2018-12-01', 39000),
+('Renault', 'Clio', 'Diesel', 'REN852', '2019-03-20', 42000),
+('Volvo', 'XC60', 'Hybrid', 'VOL951', '2020-10-05', 28000);
+
+
 DROP TABLE cars;
